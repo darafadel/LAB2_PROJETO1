bc57244b98797a1046618387444f4344f24384ed
Dara Ferreira Fadel <C3007871@fgv.edu.br>
2025-08-13 13:48:33 -0300

    3/4: função que sorteia 7 números e calcula média

--- main.py
+++ main.py
@@ -4,3 +4,16 @@ def cumprimento(texto):
 if_name_ == "_main_":
 nome_completo = "Dara Ferreira Fadel"
 print(cumprimento(nome_completo))
+import random
+def cumprimento(texto):
+    return f"Olá, {texto}"
+def media_7_numeros():
+    nums = [random.randint(1, 100) for_in range (7)]
+    media = sum(nums) / len(nums)
+    return nums,media
+if_name_ == "_main_":
+nome_completo = "Dara Ferreira Fadel"
+print(cumprimento(nome_completo))
+nums, media = media_7_numeros()
+print("Números sorteados:"nums)
+print("Média:", media)
